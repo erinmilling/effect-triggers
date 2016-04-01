171f5a6bc3b51691b4cccaae1966498e51aae4a9
erinmilling <mill0814@algonquinlive.com>
2016-04-01 10:13:15 -0400

    finished adding vars

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 var $box = $('.box');
+var $btnshowHide = $('.btn-show-hide');
+var $diamond = $('.diamond');
+var $btnMove = $('.btn-move');
+var $panel = $('.panel');
+var $btncollapseExpand = $('.btn-collapse-expand');
+var $circle = $('.circle');
+var $btnBounce = $('.btn-bounce');
+var $list = $('.list');
+var $btnAppend = $(.btn-append);
+
 
 $btnshowHide.on('click', function () {
   $box.toggleClass('appear');
